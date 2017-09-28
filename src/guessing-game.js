227ed755283616d509b0f0aa1aa02dae8138946d
Gtufc <vlad.gtufc.92@gmail.com>
2017-09-28 19:56:09 +0300

    complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
+var mid;
 class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	mid = Math.round(this.min + (this.max - this.min)/2);
+    	return mid;
     }
 
     lower() {
-
+    	this.max = mid;
     }
 
     greater() {
-
+    	this.min = mid;
     }
 }
 
